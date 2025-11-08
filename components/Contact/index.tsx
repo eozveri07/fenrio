a9393544633094e46acac9ca91f3f59cd0aaa12d
NNakreSS <serkanatmaca15@gmail.com>
2025-11-09 01:53:25 +0300

    feat: add title animation to Contact component using GSAP and restructure layout for improved presentation

--- components/Contact/index.tsx
+++ components/Contact/index.tsx
@@ -11,15 +11,30 @@ export default function Contact() {
   const containerRef = useRef<HTMLDivElement>(null);
   const leftImageRef = useRef<HTMLDivElement>(null);
   const rightImageRef = useRef<HTMLDivElement>(null);
+  const titleRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const leftImage = leftImageRef.current;
     const rightImage = rightImageRef.current;
-
-    if (!leftImage || !rightImage) return;
+    const title = titleRef.current;
+    if (!leftImage || !rightImage || !title) return;
 
     gsap.set(leftImage, { x: "-100%" });
     gsap.set(rightImage, { x: "100%" });
+    gsap.set(title, { opacity: 0, y: 100 });
+
+    gsap.to(title, {
+      opacity: 1,
+      y: 0,
+      duration: 1,
+      ease: "power2.inOut",
+      scrollTrigger: {
+        trigger: title,
+        start: "center bottom",
+        end: "bottom top",
+        scrub: 1,
+      },
+    });
 
     gsap.to(leftImage, {
       x: "0%",
@@ -49,29 +64,37 @@ export default function Contact() {
   }, []);
 
   return (
-    <div
-      ref={containerRef}
-      className="w-full h-auto grid grid-cols-2 py-6 overflow-hidden"
-    >
-      <div ref={leftImageRef} className="w-full h-full flex items-end">
-        <Image
-          src="/assets/contact/sol-el-no-holo.png"
-          alt="Contact"
-          className="object-cover w-full h-auto"
-          quality={100}
-          width={1920}
-          height={1080}
-        />
+    <div className="w-full h-auto py-6">
+      <div ref={titleRef} className="text-center text-5xl font-extrabold">
+        <span>
+          Geleceğe Açılan <br />
+          Dijital Dokunuş
+        </span>
       </div>
-      <div ref={rightImageRef} className="w-full h-full flex items-start">
-        <Image
-          src="/assets/contact/sag-robot-el-no-holo.png"
-          alt="Contact"
-          className="object-cover w-full h-auto"
-          quality={100}
-          width={1920}
-          height={1080}
-        />
+      <div
+        ref={containerRef}
+        className="w-full h-auto grid grid-cols-2 overflow-hidden"
+      >
+        <div ref={leftImageRef} className="w-full h-full flex items-end">
+          <Image
+            src="/assets/contact/sol-el-no-holo.png"
+            alt="Contact"
+            className="object-cover w-full h-auto"
+            quality={100}
+            width={1920}
+            height={1080}
+          />
+        </div>
+        <div ref={rightImageRef} className="w-full h-full flex items-start">
+          <Image
+            src="/assets/contact/sag-robot-el-no-holo.png"
+            alt="Contact"
+            className="object-cover w-full h-auto"
+            quality={100}
+            width={1920}
+            height={1080}
+          />
+        </div>
       </div>
     </div>
   );
