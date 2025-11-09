7f05731cc34eabfd7c03ccd95814bef93dd6e8c2
NNakreSS <serkanatmaca15@gmail.com>
2025-11-09 15:52:54 +0300

    feat: enhance Hero component with logo and text for improved branding and visual appeal

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -1,5 +1,27 @@
 "use client";
 
+import Image from "next/image";
+
 export default function Hero() {
-  return <div>Hero</div>;
+  return (
+    <div className="w-full h-screen flex items-center justify-center">
+      <div className="flex items-end justify-center w-full h-[200px]">
+        <div className="overflow-hidden flex items-center justify-center h-full">
+          <Image
+            src="/fenrio-logo.png"
+            alt="Fenrio Logo"
+            className="object-cover w-auto h-full"
+            width={1920}
+            height={1080}
+          />
+        </div>
+        <div>
+          <h2 className="text-9xl font-bold relative z-10">fenrio</h2>
+          <p className="font-light relative z-10 uppercase text-red-800">
+            Software Marketing
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
