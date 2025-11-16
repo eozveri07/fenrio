05abd8aa68882844be636f5bd624858e78c3b93f
NNakreSS <serkanatmaca15@gmail.com>
2025-11-17 00:28:32 +0300

    refactor: enhance HeroMain component with animated shiny text and update layout styles for improved visual appeal

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
+import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
 import { NextIntlClientProvider } from "next-intl";
 
 const inter = Inter({
--- components/Hero/main.tsx
+++ components/Hero/main.tsx
@@ -4,6 +4,8 @@ import { useEffect, useRef } from "react";
 import { useHeroContext } from "./context";
 import { gsap } from "gsap";
 import { Highlighter } from "../ui/highlighter";
+import { AnimatedShinyText } from "../ui/animated-shiny-text";
+import { cn } from "@/lib/utils";
 
 export default function HeroMain() {
   const containerRef = useRef<HTMLDivElement>(null);
@@ -23,7 +25,7 @@ export default function HeroMain() {
     <section
       id="hero"
       ref={containerRef}
-      className="w-full h-[calc(100vh-200px)] flex items-center justify-center overflow-hidden absolute top-[120px] left-0 transition-opacity duration-300 opacity-0 transform-y-100"
+      className="w-full h-[calc(100vh-200px)] flex items-center justify-center overflow-hidden absolute top-[120px] left-0 transition-opacity duration-300 opacity-0 transform-y-100 dark"
     >
       <div className="w-full h-full items-center justify-center overflow-hidden grid grid-cols-1 gap-4">
         <div className="w-full h-full flex p-9 flex-col gap-4 items-center justify-center">
@@ -37,7 +39,15 @@ export default function HeroMain() {
             </Highlighter>{" "}
             güçlendiriyoruz.
           </p>
-          <h3 className="text-white mt-5">İnovate, Elovate, Dominate</h3>
+          <div
+            className={cn(
+              "group rounded-full border border-black/5 bg-neutral-100 text-base text-white transition-all ease-in hover:cursor-pointer hover:bg-neutral-200 dark:border-white/5 dark:bg-neutral-900 dark:hover:bg-neutral-800"
+            )}
+          >
+            <AnimatedShinyText className="inline-flex items-center justify-center px-4 py-1 transition ease-out hover:text-neutral-600 hover:duration-300 hover:dark:text-neutral-400">
+              <span>İnovate, Elovate, Dominate</span>
+            </AnimatedShinyText>
+          </div>
         </div>
       </div>
     </section>
